43d63e52abec14affff0cf011fbae490e5f1428b
sergey_filatov <don.filato2010@yandex.ru>
2023-08-31 20:10:59 +0300

    loading Styles

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const dropDownBtn = document.querySelector('.button--dropdown')
 const dropDownIcon = document.querySelector('.dropdown__icon')
 const dropDownList = document.querySelector('.dropdown__list')
-
+const form = document.querySelector('.form')
 
 dropDownBtn.addEventListener('click', (e) => {
     e.preventDefault()
@@ -12,4 +12,20 @@ dropDownBtn.addEventListener('click', (e) => {
 
 
 })
+
+
+dropDownList.addEventListener('click', (e) =>{
+    const itemList = e.target.innerText;
+
+    dropDownBtn.innerText = itemList;
+
+    
+})
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    console.log('form submit')
+})
+
+
 
