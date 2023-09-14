0aed732ee6624bc9f2835463160c1d9d3c013e67
sergey_filatov <don.filato2010@yandex.ru>
2023-09-14 21:44:37 +0300

    loading change file projeckt

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ const dropDownBtn = document.querySelector('.button--dropdown')
 const dropDownIcon = document.querySelector('.dropdown__icon')
 const dropDownList = document.querySelector('.dropdown__list')
 const form = document.querySelector('.form')
+const range = document.querySelector('.range-slider__percent')
+const rangeInput = document.querySelector('.range-slaider__track')
+
+function func1(){
+    if(rangeInput.value != 0){
+        range.innerHTML = rangeInput.value + ' %'
+    }
+    else{
+        range.innerHTML = ''
+    }
+    
+}
 
 dropDownBtn.addEventListener('click', (e) => {
     e.preventDefault()
